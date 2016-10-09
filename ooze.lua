badaadde497e1631730d2b199379c17bb3b14aa1
ezhh <owlecho@live.com>
2016-10-09 12:02:33 +0100

    Removed light

--- ooze.lua
+++ ooze.lua
@@ -1,8 +1,5 @@
 
--- Note: commented code was code from the original I removed to get this working. 
-
 minetest.register_node("haunted:ooze_brick", {
-	--drawtype = "firelike",
 	description = "Ooze Brick",
 	tiles = { 
 		"default_stone_brick.png",
@@ -20,22 +17,10 @@ minetest.register_node("haunted:ooze_brick", {
 			},
 		},
 	},
-	--inventory_image = "haunted_stone_basic.png",
 	paramtype = "light",
 	paramtype2 = "facedir",
-	--light_source = 5,
-	light_source = 1,
-	--walkable = true,
-	--buildable_to = true,
 	sunlight_propagates = false,
 	groups = {cracky = 2, stone = 1},
-	--groups = {dig_immediate = 3, not_in_creative_inventory = 1},
 	drop = "default:stonebrick",
-
-	--on_blast = function()
-	--end,
 })
-
-
---end
 
