dd8f400e2886f14d50d0860ec16e57709865e327
ezhh <owlecho@live.com>
2016-11-22 13:31:12 +0000

    Added override textures

--- overrides.lua
+++ overrides.lua
@@ -33,3 +33,31 @@ minetest.override_item("default:grass_2", {
 minetest.override_item("default:grass_1", {
 	tiles = {"haunted_grass_1.png"},
 })
+
+minetest.override_item("default:apple", {
+	tiles = {"haunted_apple.png"},
+})
+
+minetest.override_item("flowers:rose", {
+	tiles = {"haunted_rose.png"},
+})
+
+minetest.override_item("flowers:viola", {
+	tiles = {"haunted_viola.png"},
+})
+
+minetest.override_item("flowers:dandelion_white", {
+	tiles = {"haunted_dandelion_white.png"},
+})
+
+minetest.override_item("flowers:tulip", {
+	tiles = {"haunted_tulip.png"},
+})
+
+minetest.override_item("flowers:dandelion_yellow", {
+	tiles = {"haunted_dandelion_yellow.png"},
+})
+
+minetest.override_item("flowers:geranium", {
+	tiles = {"haunted_geranium.png"},
+})
